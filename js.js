abf1d78501972ace872f042476a4218d81e217ef
Hiperrealitrip <francocarpino@live.com>
2023-08-11 13:39:58 -0300

    All done

--- js.js
+++ js.js
@@ -1,25 +1,36 @@
-let randnum = Math.floor(Math.random() * 3) +1
 
-function getComputerChoice(randnum) {
-    if (randnum == 1) {return "Rock"}
-    else if (randnum == 2) {return "Paper"}
-    else {return "Scissors"}
+
+function game () {
+    let limit = 5
+    let player = 0
+    let computer = 0
+    for (let i = 0; i < limit; i++) {
+    let playerSelection = prompt("Rock, Paper or Scissors?")
+    let randnum = Math.floor(Math.random() * 3) +1
+    function getComputerChoice(randnum) {
+        if (randnum == 1) {return "Rock"}
+        else if (randnum == 2) {return "Paper"}
+        else {return "Scissors"}
+    }
+    let computerSelection = getComputerChoice(randnum)
+    if (singleRound(playerSelection, computerSelection) == "Player") {player = player +1}
+    else if (singleRound(playerSelection, computerSelection) == "Computer") {computer = computer +1}
+    console.log(singleRound(playerSelection, computerSelection) + " scores")
+    }
+    if (player > computer) {return console.log("Player wins")}
+    else if (computer > player) {return console.log("Computer wins")}
+    else {return console.log("It's a tie")}
 }
-
-let playerSelection = prompt("Rock, Paper or Scissors?").toUpperCase()
-console.log("U chose: " + playerSelection)
-
-let computerSelection = getComputerChoice(randnum)
-console.log("Computer chose: " + computerSelection)
 
 function singleRound (playerSelection, computerSelection) {
-    if (playerSelection == "ROCK" && computerSelection == "Scissors") {return "U win"}
-    else if (playerSelection == "ROCK" && computerSelection == "Paper") {return "U lose"}
-    else if (playerSelection == "PAPER" && computerSelection == "Rock") {return "U win"}
-    else if (playerSelection == "PAPER" && computerSelection == "Scissors") {return "U lose"}
-    else if (playerSelection == "SCISSORS" && computerSelection == "Paper") {return "U win"}
-    else if (playerSelection == "SCISSORS" && computerSelection == "Rock") {return "U lose"}
-    else if (playerSelection == computerSelection.toUpperCase()) {return "It's a tie"}
+    return (playerSelection.toUpperCase() == "ROCK" && computerSelection.toUpperCase() == "SCISSORS") ? "Player":
+    (playerSelection.toUpperCase() == "ROCK" && computerSelection.toUpperCase() == "PAPER") ? "Computer":
+    (playerSelection.toUpperCase() == "PAPER" && computerSelection.toUpperCase() == "ROCK") ? "Player":
+    (playerSelection.toUpperCase() == "PAPER" && computerSelection.toUpperCase() == "SCISSORS") ? "Computer":
+    (playerSelection.toUpperCase() == "SCISSORS" && computerSelection.toUpperCase() == "PAPER") ? "Player":
+    (playerSelection.toUpperCase() == "SCISSORS" && computerSelection.toUpperCase() == "ROCK") ? "Computer":
+    (playerSelection.toUpperCase() == computerSelection.toUpperCase()) ? "No one":
+    "Incorrect input"
 }
 
-alert(singleRound(playerSelection, computerSelection))
+game()
